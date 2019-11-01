50d85bf843456ca55b8dc7cc5337605f363a7fd3
Sultan <im.sultaan05@gmail.com>
2019-11-01 09:31:02 +0700

    migration posts

---
+++ database/migrations/2019_10_31_071050_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->index()->nullable();
+            $table->string('slug');
+            $table->string('title')->nullable()->default('Your Awesome Title');
+            $table->text('body')->nullable();
+            // $table->integer('published')->default(0);
+            $table->boolean('published')->default(false);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
